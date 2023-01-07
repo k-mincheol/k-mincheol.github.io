ae0a2df58ff43db4f2634942801ea6095e3a6ea6
kmincheol <kmc.mincheol@gmail.com>
2023-01-07 15:09:22 +0900

    자기소개 구성만 추후에 데이터 입력 경력 & 프로젝트 진행중 프로젝트 넣는중

---
+++ js/exprience_list.js
@@ -0,0 +1,54 @@
+let exprienceDatas = [
+    {
+        "name": "야근몬스터",
+        "date": "2019.08 ~ 2022.09",
+        "detail": "SI(System Integration), SM(System Management)",
+        "project_list": [
+            {
+                "name": "스스므",
+                "url": "https://ssmuniverse.com",
+                "described": "패션유튜버 최겨울 커뮤니티",
+                "detail": "패션 유튜버 최겨울 커뮤니티 사용자, 관리자페이지 프론트작업",
+                "use_skill": "html, css, javascript"
+            },
+            {
+                "name": "씨에어허브",
+                "url": "https://seairhub.com/home",
+                "described": "무역 수출 수입 서비스",
+                "detail": "무역 수출 수입 서비스 사용자, 관리자페이지 프론트작업",
+                "use_skill": "html, css, javascript"
+            },
+            {
+                "name": "메디셀프",
+                "url": "https://medi-self.com/#self",
+                "described": "맞춤형 화장품 브랜드",
+                "detail": "맞춤화장품 피부 테스트 사용자, 관리자페이지 프론트작업",
+                "use_skill": "html, css, javascript"
+            },
+        ]
+    },
+    {
+        "name": "기원킹몬스터",
+        "date": "2022.09 ~ 2023.01",
+        "detail": "SI(System Integration), SM(System Management)",
+        "project_list": [
+            {
+                "name": "스스므",
+                "described": "패션유튜버 최겨울 커뮤니티",
+                "detail": "패션 유튜버 최겨울 커뮤니티로 블라블라",
+                "use_skill": "html, css, javascript"
+            }
+        ]
+    }
+];
+
+// 1. 스마일업(사용자)
+// 3. 펫마인드캠(사용자, 관리자)
+// 4. 보이스체크(사용자, 관리자)
+// 5. 삼구미디어(관리자)
+// 6. 음악저작권협회(관리자)
+// 7. 산모피아(관리자)
+// 8. b2b(사용자, 관리자)
+// 10. 장청소굿(관리자)
+// 11. 이가그룹(관리자)
+// 12. 비트포비아(관리자)
