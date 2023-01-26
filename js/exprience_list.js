ec5ac968fe8a3acc5da1c938a82729b28e29cab1
kmincheol <kmc.mincheol@gmail.com>
2023-01-26 16:13:30 +0900

    경력 프로젝트 완료

--- js/exprience_list.js
+++ js/exprience_list.js
@@ -10,101 +10,132 @@ let exprienceDatas = [
             {
                 "name": "스스므",
                 "url": "https://ssmuniverse.com",
+                "url_admin": "susumu.adm.yageun.com",
                 "described": "패션유튜버 최겨울 커뮤니티",
-                "detail": "패션 유튜버 최겨울 커뮤니티 사용자, 관리자페이지 프론트작업",
+                "detail": "패션 유튜버 최겨울 커뮤니티 사용자, 관리자페이지 퍼블리싱 & 프론트작업",
                 "use_skill": "HTML / CSS / JAVASCRIPT"
             },
             {
                 "name": "씨에어허브",
                 "url": "https://seairhub.com/home",
+                "url_admin": "",                
                 "described": "무역 수출 수입 서비스",
-                "detail": "무역 수출 수입 서비스 사용자, 관리자페이지 프론트작업",
+                "detail": "무역 수출 수입 서비스 사용자, 관리자페이지 퍼블리싱 & 프론트작업",
                 "use_skill": "HTML / CSS / JAVASCRIPT"
             },
             {
                 "name": "메디셀프",
                 "url": "https://medi-self.com/#self",
+                "url_admin": "https://admin.mediself2.yageun.com",
                 "described": "맞춤형 화장품 브랜드",
-                "detail": "맞춤화장품 피부 테스트 사용자, 관리자페이지 프론트작업",
+                "detail": "맞춤화장품 피부 테스트 사용자, 관리자페이지 퍼블리싱 & 프론트작업",
                 "use_skill": "HTML / CSS / JAVASCRIPT"
             },
             {
                 "name": "펫마인드캠",
                 "url": "https://play.google.com/store/apps/details?id=com.pubbee.pmc&hl=ko&gl=US",
+                "url_admin": "http://admin.petmind.yageun.com/index.php/home/dashBoard",
                 "described": "반려견 심리 / 정서 분석 앱",
-                "detail": "반려견의 심리 / 정서 상태 분석 및 커뮤니티 웹앱 사용자, 관리자페이지 프론트작업",
+                "detail": "반려견의 심리 / 정서 상태 분석 및 커뮤니티 웹앱 사용자, 관리자페이지 퍼블리싱 & 프론트작업",
                 "use_skill": "HTML / CSS / JAVASCRIPT"
             },
             {
                 "name": "산모피아",
                 "url": "https://play.google.com/store/apps/details?id=com.sanmopia.app&hl=ko&gl=US&pli=1",
+                "url_admin": "",
                 "described": "프리미엄 가정방문 산후관리사 파견 서비스",
-                "detail": "산후관리사 사용자, 관리자페이지 프론트작업",
+                "detail": "산후관리사 사용자, 관리자페이지 퍼블리싱 & 프론트작업",
                 "use_skill": "HTML / CSS / JAVASCRIPT"
             },
             {
-                "name": "B2B & 트랜심",
+                "name": "아임단비",
+                "url": "https://danbee4u.co.kr/",
+                "url_admin": "",
+                "described": "산모피아 쇼핑몰",
+                "detail": "사용자페이지 퍼블리싱",
+                "use_skill": "HTML / CSS / JAVASCRIPT"
+            },
+            {
+                "name": "B2B소설, 웹툰(트랜심)",
                 "url": "https://transym.io",
-                "described": "번역솔루션",
-                "detail": "번역솔루션(텍스트파일, 웹툰) 사용자, 관리자페이지 프론트작업",
+                "url_admin": "http://ts.yageun.com/home/login",
+                "described": "텍스트, 웹툰 번역솔루션",
+                "detail": "번역솔루션(텍스트파일, 웹툰) 사용자, 관리자페이지 퍼블리싱 & 프론트작업",
                 "use_skill": "HTML / CSS / JAVASCRIPT"
             },
             {
                 "name": "smile UP",
                 // "url": "https://www.youtube.com/channel/UCn0cce5_pFt3g96daN_4IFQ",
                 "url": "",
+                "url_admin": "http://ts.yageun.com/home/login",
                 "described": "미국 스탠포드대학교 Paul Kim 교수의 오랜 연구와 노하우를 바탕으로 만들어진 질의 응답 기반 소셜 러닝 플랫폼",
-                "detail": "사용자, 관리자페이지 프론트작업(현재 서비스 종료)",
+                "detail": "사용자 페이지 퍼블리싱 & 프론트작업(현재 서비스 종료)",
                 "use_skill": "HTML / CSS / JAVASCRIPT"
             },
             {
-                "name": "비트포비아",
+                "name": "비트포비아 관리자",
                 "url": "",
+                "admin_url": "http://admin.phobia.yageun.com",
                 "described": "방탈출 카페",
-                "detail": "사용자 컨트롤 위한 관리자페이지 프론트작업",
+                "detail": "사용자 컨트롤 위한 관리자페이지 퍼블리싱 & 프론트작업",
                 "use_skill": "HTML / CSS / JAVASCRIPT"
             },
             {
                 "name": "음악저작권협회",
                 "url": "",
-                "described": "음악저작권협회 관리자",
-                "detail": "음악 저작권 관련 관리페이지 프론트작업",
+                "admin_url": "riak.yageun.com",
+                "described": "음악저작권관리",
+                "detail": "음악 저작권 관련 관리페이지 퍼블리싱 & 프론트작업",
                 "use_skill": "HTML / CSS / JAVASCRIPT"
             },
-            
+            {
+                "name": "이가그룹-콘체크",
+                "url": "",
+                "admin_url": "http://egacon.yageun.com/index.php/home/login",
+                "described": "건축사무소 및 인력 관리",
+                "detail": "관리페이지 퍼블리싱 & 프론트작업",
+                "use_skill": "HTML / CSS / JAVASCRIPT"
+            },
+            {
+                "name": "보이스체크",
+                "url": "",
+                "admin_url": "",
+                "described": "목소리발성 교육서비스 제공",
+                "detail": "사용자, 관리자페이지 퍼블리싱 & 프론트작업",
+                "use_skill": "HTML / CSS / JAVASCRIPT"
+            },
+            {
+                "name": "삼구미디어",
+                "url": "http://samkoomedia.yamon.io/",
+                "admin_url": "",
+                "described": "삼구아이앤씨 사내게시판 홍보영상, 이벤트, 알림정보, 날씨, 뉴스 등",
+                "detail": "퍼블리싱 & 프론트작업",
+                "use_skill": "HTML / CSS / JAVASCRIPT"
+            },
+            {
+                "name": "장시원",
+                "url": "",
+                "admin_url": "http://janggood.yageun.com/index.php/home/login",
+                "described": "병원, 식단, 약 관련 관리",
+                "detail": "퍼블리싱 & 프론트작업",
+                "use_skill": "HTML / CSS / JAVASCRIPT"
+            },
         ]
     },
-    {
-        "id": 2,
-        "name": "기원킹몬스터",
-        "date": "2022.09 ~ 2023.01",
-        "detail": "SI(System Integration), SM(System Management)",
-        "description": "Xd, Zeplin 으로 받은 디자인을 받아 퍼블리싱 & 프론트엔드 작업 진행하였습니다.",
-        "skills": "Html · css · javascript · Git · Xd · Zeplin · Microsoft teams · slack · trello · vscode",
-        "project_list": [
-            {
-                "name": "기원킹킹킹",
-                "described": "패션유튜버 최겨울 커뮤니티",
-                "detail": "패션 유튜버 최겨울 커뮤니티로 블라블라",
-                "use_skill": "HTML / CSS / JAVASCRIPT"
-            }
-        ]
-    }
+    // {
+    //     "id": 2,
+    //     "name": "기원킹몬스터",
+    //     "date": "2022.09 ~ 2023.01",
+    //     "detail": "SI(System Integration), SM(System Management)",
+    //     "description": "Xd, Zeplin 으로 받은 디자인을 받아 퍼블리싱 & 프론트엔드 작업 진행하였습니다.",
+    //     "skills": "Html · css · javascript · Git · Xd · Zeplin · Microsoft teams · slack · trello · vscode",
+    //     "project_list": [
+    //         {
+    //             "name": "기원킹킹킹",
+    //             "described": "패션유튜버 최겨울 커뮤니티",
+    //             "detail": "패션 유튜버 최겨울 커뮤니티로 블라블라123123",
+    //             "use_skill": "HTML / CSS / JAVASCRIPT"
+    //         }
+    //     ]
+    // }
 ];
-
-
-// 4. 보이스체크(사용자, 관리자)
-// 5. 삼구미디어(관리자)
-// 10. 장청소굿(관리자)
-// 11. 이가그룹(관리자)
-
-// 6. 음악저작권협회(관리자)
-// 12. 비트포비아(관리자)
-// 1. 스마일업(사용자)
-// 스스므
-// 씨에어허브
-// 메디셀프
-// 3. 펫마인드캠(사용자, 관리자) 
-// 산모피아
-// b2b & 트랜심(사용자, 관리자)
-// 
